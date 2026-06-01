bab3065d9b6cf9ebf13da133d72d7c893bfa5af5
angel2334-spec <chidinmaangel32@gmail.com>
2026-06-01 04:04:21 -0700

    i added my food recipe app.

---
+++ assignment/egobudike-angel/food reciepe/script.js
@@ -0,0 +1,22 @@
+const cards =document.querySelectorAll(".recipe-card");
+
+cards.forEach(card=> {}
+
+ card.addEventListener("click",() => {
+     
+    card.style.transform ="scale(0.97)";
+
+    setTimeout(() => {
+       card.style.transform ="scale(1)" ;
+    }, 150);
+ }   
+
+
+
+
+
+
+
+
+    
+
